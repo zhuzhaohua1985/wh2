e6fad4448dfe540b90565d1e08943a75a36e8ff4
gxie <gxie@gxie-PC>
2017-04-28 00:30:42 +0800

    获取接口服务实时网吧在线数

---
+++ QCMS/src/main/java/com/iss/entity/AreasBarEntity.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.iss.entity;
+
+/**
+ * @author gxie
+ *
+ */
+public class AreasBarEntity {
+
+	private String barId;
+	private String barName;
+	private String offline;
+	private String online;
+	private String osVersion;
+	private String valid;
+	
+	public String getBarId() {
+		return barId;
+	}
+	public void setBarId(String barId) {
+		this.barId = barId;
+	}
+	public String getBarName() {
+		return barName;
+	}
+	public void setBarName(String barName) {
+		this.barName = barName;
+	}
+	public String getOffline() {
+		return offline;
+	}
+	public void setOffline(String offline) {
+		this.offline = offline;
+	}
+	public String getOnline() {
+		return online;
+	}
+	public void setOnline(String online) {
+		this.online = online;
+	}
+	public String getOsVersion() {
+		return osVersion;
+	}
+	public void setOsVersion(String osVersion) {
+		this.osVersion = osVersion;
+	}
+	public String getValid() {
+		return valid;
+	}
+	public void setValid(String valid) {
+		this.valid = valid;
+	}
+	
+}
